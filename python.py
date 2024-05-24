527cf07bb1fee3b05f797f65974d33ec52172ad5
ZINLAK <108427946+ZinLak@users.noreply.github.com>
2024-05-24 23:26:56 +0300

    Update python.py

--- python.py
+++ python.py
@@ -7,89 +7,79 @@
 
 Вариант 26. F(1) = 1; G(1) = 1; F(n) =(-1)**n * (F(n–1) – G(n–1)), G(n) = F(n–1) + 2*G(n–1) /(2n)!, при n >=2
 """
+
 import time
+import matplotlib.pyplot as plt
 
-class SingleItemCache:
-    def __init__(self):
-        self.item = None
+factorials = {'key': 0, 'item': 1}
+cache_F = {}
+cache_G = {}
+cache_alternationSign = {'sign': None}
 
-    def add(self, item):
-        self.item = item
+timer=[]
+timer_rec=[]
 
-    def get(self):
-        return self.item
-
-# Создаем экземпляр кэша
-cache = SingleItemCache()
-cache_F = SingleItemCache()
-cache_G = SingleItemCache()
-factorials = SingleItemCache()
-
-def memoize(func):
-    cached_results = {}
-    def wrapper(n, cache):
-        if n not in cached_results:
-            cached_results[n] = func(n, cache)
-        return cached_results[n]
-    return wrapper
-
-@memoize
-def factorial(n, cache):
-    if n == 0:
-        return 1
-    if n != cache.get():
-        result = 1
-        for i in range(1, n + 1):
-            result *= i
-        cache.add(n)
-        cache.add(result)
-    return cache.get()
-
-@memoize
-def recursive_F(n, cache):
-    if n == 1:
-        return 1
-    if n != cache.get():
-        if n % 2 == 0:
-            cache.add((-1) ** 2 * (recursive_F(n - 1, cache) - recursive_G(n - 1, cache)))
+def factorial(n):
+    if n != factorials['key']:
+        if factorials['item'] is None or (n != factorials['key'] + 1 and n != factorials['key'] - 1):
+            result = 1
+            for i in range(1, n + 1):
+                result *= i
+            factorials['key'], factorials['item'] = n, result
+            return factorials['item']
         else:
-            cache.add((-1) ** 1 * (recursive_F(n - 1, cache) - recursive_G(n - 1, cache)))
-        return cache.get()
+            if n > factorials['key']:
+                factorials['key'] += 1
+                factorials['item'] *= factorials['key']
+            elif n < factorials['key']:
+                factorials['item'] //= factorials['key']
+                factorials['key'] -= 1
+            return factorials['item']
+    else:
+        return factorials['item']
 
-@memoize
-def recursive_G(n, cache):
+def recursive_F(n):
     if n == 1:
         return 1
-    if n != cache.get():
-        cache.add(recursive_F(n - 1, cache) + 2 * recursive_G(n - 1, cache) / factorial(2 * n, factorials))
-    return cache.get()
+    if n not in cache_F:
+        cache_alternationSign['sign'] = -1 if n % 2 == 1 else 1
+        cache_F.clear()
+        cache_F[n] = cache_alternationSign['sign'] * (recursive_F(n - 1) - recursive_G(n - 1))
+    return cache_F[n]
+
+def recursive_G(n):
+    if n == 1:
+        return 1
+    if n not in cache_G:
+        cache_G.clear()
+        cache_G[n] = recursive_F(n - 1) + 2 * recursive_G(n - 1) / factorial(2 * n)
+    return cache_G[n]
 
 def iterative_F(n):
+    if n == 1:
+        return 1
     f, g = 1, 1
     for i in range(2, n + 1):
-        if n % 2 == 0:
-            f, g = (-1) ** 2 * (f - g), f + 2 * g / factorial(2 * i, factorials)
-        else:
-            f, g = (-1) ** 1 * (f - g), f + 2 * g / factorial(2 * i, factorials)
+        sign = -1 if i % 2 == 1 else 1
+        f, g = sign * (f - g), f + 2 * g / factorial(2 * i)
     return f
 
 def iterative_G(n):
+    if n == 1:
+        return 1
     f, g = 1, 1
     for i in range(2, n + 1):
-        if n % 2 == 0:
-            f, g = (-1) ** 2 * (f - g), f + 2 * g / factorial(2 * i, factorials)
-        else:
-            f, g = (-1) ** 1 * (f - g), f + 2 * g / factorial(2 * i, factorials)
+        sign = -1 if i % 2 == 1 else 1
+        f, g = sign * (f - g), f + 2 * g / factorial(2 * i)
     return g
 
 n = int(input('Введите натуральное число: '))
-while not (2 <= n):
-    print('Число не является допустимым значением.')
-    n = int(input('Повторите ввод:'))
+while n < 2:
+    n = int(input('Введите натуральное число (min 2): '))
 
 start_time = time.time()
-recursive_f_result = recursive_F(n, cache_F)
-recursive_g_result = recursive_G(n, cache_G)
+recursive_f_result = recursive_F(n)
+recursive_g_result = recursive_G(n)
 recursive_time = time.time() - start_time
 
 start_time = time.time()
@@ -101,3 +91,23 @@ print("Recursive F({}) = {}, time: {:.6f} seconds".format(n, recursive_f_result,
 print("Recursive G({}) = {}, time: {:.6f} seconds".format(n, recursive_g_result, recursive_time))
 print("Iterative F({}) = {}, time: {:.6f} seconds".format(n, iterative_f_result, iterative_time))
 print("Iterative G({}) = {}, time: {:.6f} seconds".format(n, iterative_g_result, iterative_time))
+
+graf = list(range(1, n+1))
+
+for i in graf:
+    start = time.time()
+    result = iterative_F(i)
+    end = time.time()
+    timer.append(end-start)
+    start_rec = time.time()
+    res = recursive_F(i)
+    end_rec = time.time()
+    timer_rec.append(end_rec-start_rec)
+
+plt.plot(graf, timer, label='Итерационная функция.')
+plt.plot(graf, timer_rec, label='Рекусионная функция.')
+plt.legend(loc=2)
+
+plt.xlabel('Значение n')
+plt.ylabel('Время выполнения (c)')
+plt.show()
